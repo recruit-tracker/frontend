a27cddbd0a68170e5422535c8f9974e40757f2ce
mitchell_kimbell <mfkimbell@gmail.com>
2024-03-03 01:13:15 -0600

    dropdown works

--- recruit-tracker/src/components/HrSingleProfile/HrSingleProfile.js
+++ recruit-tracker/src/components/HrSingleProfile/HrSingleProfile.js
@@ -25,15 +25,13 @@ import EditIcon from "@mui/icons-material/Edit";
 import AddIcon from "@mui/icons-material/Add";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import DescriptionIcon from "@mui/icons-material/Description";
-import { students } from "../../testData/testStudents"; // Adjust the import path as necessary
+import { API_URL } from "../../constants";
 
 const HrSingleProfile = () => {
   const location = useLocation();
   const { student: originalStudent } = location.state || {};
   const { _id, ...studentWithoutId } = originalStudent || {};
 
-  console.log("student", studentWithoutId);
-  console.log("previous", students[0]);
   const [profileInfo, setProfileInfo] = useState({
     ...studentWithoutId,
     feedback: Array.isArray(studentWithoutId.feedback)
@@ -68,105 +66,88 @@ const HrSingleProfile = () => {
 
   const handleSaveChanges = () => {
     console.log("Saving changes", profileInfo);
-
-    // Implement save functionality here
+    fetch(API_URL + "/student/update", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        user: profileInfo,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data))
+      .catch((error) => console.error("Error:", error));
   };
 
   return (
     <div style={{ display: "flex", flexDirection: "column", height: "100vh" }}>
       <HrHeaderSingle />
-      <div
-        className="main-content"
-        style={{
-          flexGrow: 1,
-          overflowY: "auto",
-          padding: "24px",
-          marginTop: "43px",
-        }}
-      >
-        <Typography variant="h4" gutterBottom>
-          Profile Information
-        </Typography>
+      <div className="main-content" style={{ flexGrow: 1, overflowY: "auto", padding: "24px", marginTop: "43px" }}>
+        <Typography variant="h4" gutterBottom>Profile Information</Typography>
         <Grid container spacing={2} style={{ marginBottom: "20px" }}>
           {Object.entries(profileInfo).map(([key, value]) => {
-            const isSelectField = key === "stage" || key === "position";
+            if (key === "id" || key === "feedback") {
+              return null; // Skip these fields
+            }
+
             return (
-              key !== "id" &&
-              key !== "feedback" && (
-                <Grid item xs={12} sm={6} key={key}>
-                  <FormControl fullWidth variant="outlined">
-                    {isSelectField ? (
-                      <>
-                        <InputLabel>
-                          {key.charAt(0).toUpperCase() + key.slice(1)}
-                        </InputLabel>
-                        <Select
-                          value={value}
-                          onChange={(e) => handleFieldChange(e, key)}
-                          label={key.charAt(0).toUpperCase() + key.slice(1)}
-                        >
-                          <MenuItem value="">
-                            <em>None</em>
-                          </MenuItem>
-                          {key === "stage" ? (
-                            <>
-                              <MenuItem value="Applying">Applying</MenuItem>
-                              <MenuItem value="Reviewed">Reviewed</MenuItem>
-                              <MenuItem value="Interviewed">
-                                Interviewed
-                              </MenuItem>
-                              <MenuItem value="Offered">Offered</MenuItem>
-                            </>
-                          ) : (
-                            <>
-                              <MenuItem value="Full Time">Full Time</MenuItem>
-                              <MenuItem value="Part Time">Part Time</MenuItem>
-                              <MenuItem value="Intern">Intern</MenuItem>
-                            </>
-                          )}
-                        </Select>
-                      </>
-                    ) : (
-                      <TextField
-                        fullWidth
-                        label={key.charAt(0).toUpperCase() + key.slice(1)}
-                        value={value}
+              <Grid item xs={12} sm={6} key={key}>
+                <FormControl fullWidth variant="outlined">
+                  {key === "stage" || key === "position" ? (
+                    <>
+                      <InputLabel>{key.charAt(0).toUpperCase() + key.slice(1)}</InputLabel>
+                      <Select
+                        value={value || ""}
                         onChange={(e) => handleFieldChange(e, key)}
-                        variant="outlined"
-                        InputProps={{
-                          startAdornment:
-                            key === "linkedin" ? (
-                              <InputAdornment position="start">
-                                <IconButton
-                                  onClick={() => window.open(value, "_blank")}
-                                >
-                                  <LinkedInIcon />
-                                </IconButton>
-                              </InputAdornment>
-                            ) : key === "resume" ? (
-                              <InputAdornment position="start">
-                                <IconButton
-                                  onClick={() => window.open(value, "_blank")}
-                                >
-                                  <DescriptionIcon />
-                                </IconButton>
-                              </InputAdornment>
-                            ) : null,
-                          endAdornment: (
-                            <InputAdornment position="end">
-                              <Tooltip title="Edit">
-                                <IconButton>
-                                  <EditIcon />
-                                </IconButton>
-                              </Tooltip>
-                            </InputAdornment>
-                          ),
-                        }}
-                      />
-                    )}
-                  </FormControl>
-                </Grid>
-              )
+                        label={key.charAt(0).toUpperCase() + key.slice(1)}
+                      >
+                        {key === "stage" ? (
+                          ["Applying", "Reviewed", "Interviewed", "Offered"].map(option => (
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                          ))
+                        ) : (
+                          ["Full Time", "Part Time", "Intern"].map(option => (
+                            <MenuItem key={option} value={option}>{option}</MenuItem>
+                          ))
+                        )}
+                      </Select>
+                    </>
+                  ) : (
+                    <TextField
+                      fullWidth
+                      label={key.charAt(0).toUpperCase() + key.slice(1)}
+                      value={value}
+                      onChange={(e) => handleFieldChange(e, key)}
+                      variant="outlined"
+                      InputProps={{
+                        startAdornment: key === "linkedin" ? (
+                          <InputAdornment position="start">
+                            <IconButton onClick={() => window.open(value, "_blank")}>
+                              <LinkedInIcon />
+                            </IconButton>
+                          </InputAdornment>
+                        ) : key === "resume" ? (
+                          <InputAdornment position="start">
+                            <IconButton onClick={() => window.open(value, "_blank")}>
+                              <DescriptionIcon />
+                            </IconButton>
+                          </InputAdornment>
+                        ) : null,
+                        endAdornment: key !== "email" && key !== "name" ? (
+                          <InputAdornment position="end">
+                            <Tooltip title="Edit">
+                              <IconButton>
+                                <EditIcon />
+                              </IconButton>
+                            </Tooltip>
+                          </InputAdornment>
+                        ) : null,
+                      }}
+                    />
+                  )}
+                </FormControl>
+              </Grid>
             );
           })}
         </Grid>
